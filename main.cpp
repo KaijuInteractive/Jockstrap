aebe4a6258d9dc6d574499f2754de5340ecacb5b
Kaiju <rgonyea2012@gmail.com>
2026-09-15 18:28:11 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Jockstrap Programming Language" << endl;
+    cout << "Version 0.1" << endl;
+    cout << endl;
+
+    cout << "Securing package..." << endl;
+    cout << "Everything looks snug." << endl;
+
+    return 0;
+}
